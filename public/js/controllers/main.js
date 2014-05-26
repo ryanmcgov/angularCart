60d89c86ab097974ff2d9afe53c180d963a23708
Ryan McGovern <ryanmcgovern@Ryans-MacBook-Air.local>
2014-05-26 17:43:42 -0400

    shop front

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -27,14 +27,15 @@ angular.module('shoppingBlog')
   })
   .controller('CheckoutCtrl', function ($scope) {
     $scope.itemCheckout = [];
+    $scope.products = ProductsSvc.query();
 
-    $scope.addToCart = function(item) {
-        $scope.itemCheckout.push(item);
+    $scope.addToCart = function(product) {
+        $scope.itemCheckout.push(product);
          
     };
 
-    $scope.removeFromCart = function(item) {
-        var toRemove = $scope.itemCheckout.indexOf(item);
+    $scope.removeFromCart = function(product) {
+        var toRemove = $scope.itemCheckout.indexOf(product);
         console.log(toRemove);
 
         $scope.itemCheckout.splice(toRemove, 1);
@@ -47,6 +48,18 @@ angular.module('shoppingBlog')
       ProductsSvc.create(product);
       $location.path('/admin');
     };
+
+    $scope.post = ProductSvc.show({ id: $routeParams.id });
+    $scope.delete = function() { 
+      PostSvc.delete({ id: $routeParams.id });
+      $location.path('/admin');
+    };
+    $scope.edit = function() {
+      PostSvc.edit($scope.post);
+      $location.path('/admin');
+    };
+
+  })
 
     $scope.products = ProductsSvc.query();
 
